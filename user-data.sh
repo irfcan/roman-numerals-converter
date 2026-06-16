ee38504021272e88e647dd652add40db74e80460
Irfan Canan <irfancanan@gmail.com>
2026-06-16 11:32:54 +0200

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -3,7 +3,7 @@ yum update -y
 yum install python3-pip -y
 pip3 install Flask
 cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/guile-clarusway/roman-numerals-converter/refs/heads/main/templates/index.html
-wget -P templates https://raw.githubusercontent.com/guile-clarusway/roman-numerals-converter/refs/heads/main/templates/result.html
-wget https://raw.githubusercontent.com/guile-clarusway/roman-numerals-converter/refs/heads/main/app.py
+wget -P templates https://raw.githubusercontent.com/irfcan/roman-numerals-converter/refs/heads/main/templates/index.html
+wget -P templates https://raw.githubusercontent.com/irfcan/roman-numerals-converter/refs/heads/main/templates/result.html
+wget https://raw.githubusercontent.com/irfcan/roman-numerals-converter/refs/heads/main/app.py
 python3 app.py
